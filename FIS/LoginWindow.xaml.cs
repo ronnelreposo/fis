d4f2d98b13ce5e14445ce35863e996a97dbae0cd
ronnelreposo <ronnel.reposo@gmail.com>
2017-11-25 22:04:17 -0800

    Refactoring LogIn Logic
    Code Changes
    
     1. The subscription of noErrorField no longer uses **async**, instead it uses **fromAsync**.
     1. Added **encodedPasswordHash** for composing direct encoded password hash.
     1. Added **retrievePasswordHash** for composing direct retrieval of storedPasswordhash.
     - Added Setup for SignIn Reactive Engine composed from (1).

--- FIS/LoginWindow.xaml.cs
+++ FIS/LoginWindow.xaml.cs
@@ -13,6 +13,7 @@ using static FIS.Lib.Option<string>;
 using static FIS.Lib.DatabaseQuery;
 using static FIS.Lib.Util;
 using System.Reactive.Linq;
+using System.Reactive;
 
 namespace FIS
 {
@@ -22,6 +23,91 @@ namespace FIS
         {
             InitializeComponent();
 
+            setUpReactiveEngine();
+
+        } /* end constructor. */
+
+        /// <summary>
+        /// Encodes the given password to Password Hash.
+        /// </summary>
+        /// <param name="dataRow">The DataRow in which the SaltIndex can be retrieved.</param>
+        /// <param name="password">The given Password to be hashed.</param>
+        /// <param name="SaltColIndex">The index coloumn of the DataRow in which the salt can be retrieved.</param>
+        /// <returns>Encoded: Password Hash</returns>
+        string encodedPasswordHash (DataRow dataRow, string password, int SaltColIndex)
+        {
+            var salt = new Guid(dataRow[SaltColIndex].ToString());
+            var passwordHash = DataHash(data: password, salt: salt);
+            var strPasswordHash = ToBase64String(inArray: passwordHash.Item1);
+
+            return strPasswordHash;
+        } /* end encodePasswordHash */
+
+        /// <summary>
+        /// Retrieves the Stored Hash Password in the DataRow given the PasswordColIndex.
+        /// </summary>
+        /// <param name="dataRow">The given DataRow in which the Password Hash can be found.</param>
+        /// <param name="PasswordColIndex">The Coloumn Index of the given Row.</param>
+        /// <returns>Retrieved: Password Hash</returns>
+        string retrievePasswordHash (DataRow dataRow, int PasswordColIndex)
+        {
+            var storedPasswordHash = dataRow[PasswordColIndex].ToString();
+
+            return storedPasswordHash;
+        } /* end retrievePasswordHash. */
+
+        /// <summary>
+        /// SignIn Reactive Engine
+        /// </summary>
+        /// <param name="username">The given username</param>
+        /// <param name="password">The given password</param>
+        /// <returns>Unit</returns>
+        Unit signin (string username, string password)
+        {
+            var sQuery = FromAsync(task => QueryAsync(Procedure: "get_faculty_account",
+                      paramValue: Create("username", username as object),
+                      onFail: ex => MessageBox.Show(ex.Message)));
+
+            var sQueryDataTable = from command in sQuery
+                                  select new DataTable().FillWithCommand(command);
+
+            var sQueryHasRows = from dataTable in sQueryDataTable
+                                where dataTable.HasRows()
+                                select dataTable;
+
+            var sQueryToPasswordMatch = from dataTable in sQueryHasRows
+                                        let firstRow = dataTable.Rows[0]
+                                        let strPasswordHash = encodedPasswordHash(firstRow, password, SaltColIndex: 3)
+                                        let storedPasswordHash = retrievePasswordHash(firstRow, PasswordColIndex: 2)
+                                        let isPasswordHashMatch = strPasswordHash.Equals(storedPasswordHash)
+                                        select isPasswordHashMatch;
+
+            var sPasswordMatch = from isPasswordMatch in sQueryToPasswordMatch
+                                 where isPasswordMatch
+                                 select new MainWindow();
+            sPasswordMatch.Subscribe(mainWindow => HideAndShow(this)(mainWindow));
+
+            var sPasswordNotMatch = from isPasswordMatch in sQueryToPasswordMatch
+                                 where !isPasswordMatch
+                                 select this;
+            sPasswordNotMatch.Subscribe(@this => ShowMessage(
+                text: "Your password is incorrect",
+                title: "Incorrect Password",
+                button: MessageBoxButton.OK,
+                owner: @this));
+
+            var sQueryHasNoRows = from dataTable in sQueryDataTable
+                                  where !dataTable.HasRows()
+                                  select this;
+            sQueryHasNoRows.Subscribe(@this => ShowMessage(text: "Username is not registered.",
+                title: "unregistered username",
+                button: MessageBoxButton.OK, owner: @this));
+
+            return Unit.Default;
+        } /* signin. */
+
+        Unit setUpReactiveEngine()
+        {
             var sLoginClick = loginButton.StreamClickEvent();
 
             var sRegisterClick = registerButton.StreamClickEvent();
@@ -69,48 +155,9 @@ namespace FIS
               let errorField = fields.ErrorField
               where errorField.IsNone
               select fields )
-            .Subscribe(async fields => await QueryAsync(
-                      Procedure: "get_faculty_account",
-                      paramValue: Create("username", fields.Username as object),
-                      onFail: ex => MessageBox.Show(ex.Message),
-                      onSuccess: command =>
-                      {
-                          var filledDataTable = new DataTable().FillWithCommand(command);
+            .Subscribe(fields => signin(fields.Username, fields.Password));
 
-                          if ( !filledDataTable.HasRows() )
-                          {
-                              ShowMessage(
-                                  text: "Username is not registered.",
-                                  title: "unregistered username",
-                                  button: MessageBoxButton.OK,
-                                  owner: this);
-                              return;
-                          }
-
-                          var firstRow = filledDataTable.Rows[0];
-
-                          var SaltRowIndex = 3;
-                          var salt = new Guid(firstRow[SaltRowIndex].ToString());
-
-                          var password_hash = DataHash(data: fields.Password, salt: salt);
-                          var str_password_hash = ToBase64String(inArray: password_hash.Item1);
-
-                          var PasswordRowIndex = 2;
-                          var DBpasswordHash = firstRow[PasswordRowIndex].ToString();
-
-                          var isPasswordHashMatch = str_password_hash.Equals(DBpasswordHash);
-
-                          if ( !isPasswordHashMatch )
-                          {
-                              ShowMessage(
-                                  text: "Your password is incorrect",
-                                  title: "Incorrect Password",
-                                  button: MessageBoxButton.OK,
-                                  owner: this);
-                              return;
-                          }
-                          HideAndShow(this)(new MainWindow());
-                      })); /* end subsciption. */
-        } /* end constructor. */
+            return Unit.Default;
+        } /* end setUpReactiveEngine. */
     } /* end class. */
 } /* end namespace. */
