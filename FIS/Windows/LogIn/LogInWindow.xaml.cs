66c5a113bab0468f433dda00eec16bd5e70c9630
ronnelreposo <ronnel.reposo@gmail.com>
2017-11-27 11:16:10 -0800

    Refactoring LogInWindow
    MainWindow as parameter is removed,
    this reflects the changes from the LogInControls class.

--- FIS/Windows/LogIn/LogInWindow.xaml.cs
+++ FIS/Windows/LogIn/LogInWindow.xaml.cs
@@ -16,7 +16,6 @@ namespace FIS.Windows.LogIn
                     PasswordPasswordBox: passwordPasswordBox,
                     RegisterButton: registerButton,
                     LogInButton: loginButton,
-                    MainWindow: new MainWindow(),
                     LogInWindow: this);
 
             LogInReactiveEngine
