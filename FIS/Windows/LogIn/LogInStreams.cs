9811b8d02bc16f02eac6a54a230181e8385e8bd9
ronnelreposo <ronnel.reposo@gmail.com>
2018-01-16 14:58:02 +0800

    Rename row checking availability to Username not registered
    Changes:
     The row checking availability is replaced with Username not registered.
     Code Documentation in each Stream.

--- FIS/Windows/LogIn/LogInStreams.cs
+++ FIS/Windows/LogIn/LogInStreams.cs
@@ -3,26 +3,40 @@ using System;
 
 namespace FIS.Windows.LogIn
 {
+    /// <summary>
+    /// Represents Streams on LogIn
+    /// </summary>
     struct LogInStreams
     {
+        /// <summary>
+        /// Stream of Modern Window on password match.
+        /// </summary>
         internal IObservable<ModernWindow> SPasswordMatch { get; private set; }
+
+        /// <summary>
+        /// Stream of Modern Window on password does not match.
+        /// </summary>
         internal IObservable<ModernWindow> SPasswordNotMatch { get; private set; }
-        internal IObservable<ModernWindow> SQueryHasNoRows { get; private set; }
+
+        /// <summary>
+        /// Stream of Modern Window on Username that has not been registered.
+        /// </summary>
+        internal IObservable<ModernWindow> SUsernameNotRegistered { get; private set; }
 
         LogInStreams (
             IObservable<ModernWindow> SPasswordMatch,
             IObservable<ModernWindow> SPasswordNotMatch,
-            IObservable<ModernWindow> SQueryHasNoRows)
+            IObservable<ModernWindow> SUsernameNotRegistered)
         {
             this.SPasswordMatch = SPasswordMatch;
             this.SPasswordNotMatch = SPasswordNotMatch;
-            this.SQueryHasNoRows = SQueryHasNoRows;
+            this.SUsernameNotRegistered = SUsernameNotRegistered;
         }
 
         internal static LogInStreams Create(
             IObservable<ModernWindow> SPasswordMatch,
             IObservable<ModernWindow> SPasswordNotMatch,
-            IObservable<ModernWindow> SQueryHasNoRows) =>
-        new LogInStreams(SPasswordMatch, SPasswordNotMatch, SQueryHasNoRows);
+            IObservable<ModernWindow> SUsernameNotRegistered) =>
+            new LogInStreams(SPasswordMatch, SPasswordNotMatch, SUsernameNotRegistered);
     }
 }
