9866d74a672d678f71939776181f17222bc53895
ronnelreposo <ronnel.reposo@gmail.com>
2018-01-16 18:50:32 +0800

    Rename FacultyRec To Faculty

---
+++ FIS/Records/Faculty.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FIS.Records
+{
+    /// <summary>
+    /// Represents a Single Faculty Record.
+    /// </summary>
+    internal struct Faculty
+    {
+        /// <summary>
+        /// Personal record of the faculty.
+        /// </summary>
+        internal PersonalRec PersonalRec { get; private set; }
+
+        /// <summary>
+        /// Unique Identification of the faculty.
+        /// </summary>
+        internal int ID { get; private set; }
+
+        /// <summary>
+        /// Date hired of the faculty.
+        /// </summary>
+        internal DateTime DateHired { get; private set; }
+
+        /// <summary>
+        /// The current status of the faculty.
+        /// </summary>
+        internal string Status { get; private set; }
+
+        Faculty (int ID, PersonalRec PersonalRec, DateTime DateHired, string Status)
+        {
+            this.ID = ID;
+            this.PersonalRec = PersonalRec;
+            this.DateHired = DateHired;
+            this.Status = Status;
+        }
+
+        internal static Faculty Create (int ID, PersonalRec PersonalRec, DateTime DateHired, string Status) =>
+            new Faculty(ID: ID, PersonalRec: PersonalRec, DateHired: DateHired, Status: Status);
+    }
+}
