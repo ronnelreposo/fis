56e6ce33dc8eb2caeeca2be02e0c543b197a4d89
ronnelreposo <ronnel.reposo@gmail.com>
2018-01-21 20:28:10 +0800

    Add UserAccount

---
+++ FIS/Records/UserAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace FIS.Records
+{
+    /// <summary>
+    /// Represents a single User Account
+    /// </summary>
+    struct UserAccount
+    {
+        internal string Username { get; private set; }
+        internal string EncryptedPassword { get; private set; }
+        internal string PasswordSalt { get; private set; }
+
+        UserAccount(string Username, string EncryptedPassword, string PasswordSalt)
+        {
+            this.Username = Username;
+            this.EncryptedPassword = EncryptedPassword;
+            this.PasswordSalt = PasswordSalt;
+        }
+
+        internal static UserAccount Create(string Username, string EncryptedPassword, string PasswordSalt) =>
+            new UserAccount(Username, EncryptedPassword, PasswordSalt);
+    }
+}
